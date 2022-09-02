6de19968be3807871ad6e258e354ee2b1b31a9be
Hakki60 <112636605+Hakki60@users.noreply.github.com>
2022-09-02 14:04:54 +0200

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,33 +1,60 @@
+<?php
 
+// Replace this with your own email address
+$to = 'bhakki58@gmail.com';
+
+function url(){
+  return sprintf(
+    "%s://%s",
+    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+    $_SERVER['SERVER_NAME']
+  );
+}
+
+if($_POST) {
+
+   $name = trim(stripslashes($_POST['name']));
+   $email = trim(stripslashes($_POST['email']));
+   $subject = trim(stripslashes($_POST['subject']));
+   $contact_message = trim(stripslashes($_POST['message']));
+
+   $phone = trim(stripslashes($_POST['phone']));
+   $company = trim(stripslashes($_POST['company']));
+
+   if ($phone == '') { $phone = "N/A"; }
+   if ($company == '') { $company = "N/A"; }
+
+   
+	if ($subject == '') { $subject = "Contact Form Submission"; }
+
+   // Set Message
+   $message .= "Email from: " . $name . "<br />";
+	 $message .= "Email address: " . $email . "<br />";
+   $message .= "Phone: " . $phone . "<br />";
+   $message .= "Company: " . $company . "<br />";
+   $message .= "Message: <br />";
+   $message .= nl2br($contact_message);
+   $message .= "<br /> ----- <br /> This email was sent from your site " . url() . " contact form. <br />";
+
+   // Set From: header
+   $from =  $name . " <" . $email . ">";
+
+   // Email Headers
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+   ini_set("sendmail_from", $to); // for windows server
+   $mail = mail($to, $subject, $message, $headers);
+
+	if ($mail) { echo "OK"; }
+   else { echo "Something went wrong. Please try again."; }
+
+}
+
+?>
     
-  <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
-
- */
-require_once './vendor/autoload.php';
-
-use FormGuide\Handlx\FormHandler;
-
-
-$pp = new FormHandler();
-
-$validator = $pp->getValidator();
-$validator->fields(['name','email'])->areRequired()->maxLength(50);
-$validator->field('email')->isEmail();
-$validator->field('message')->maxLength(6000);
-
-
-
-
-$pp->sendEmailTo('bhakki58@gmail.com'); // ← Your email here
-
-echo $pp->process($_POST);
-
-
  
   
   
